ba557657be0f7c40ce8b59f11813681fc8285a6d
Prize-winner <1006430827@qq.com>
2022-11-24 22:49:41 +0800

    第1次提交c2

--- crm/src/main/java/cn/itcast/core/controller/CustomerController.java
+++ crm/src/main/java/cn/itcast/core/controller/CustomerController.java
@@ -2,6 +2,7 @@ package cn.itcast.core.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.testng.reporters.jq.Main;
 
 @Controller
 @RequestMapping("/customer")
@@ -12,6 +13,8 @@ public class CustomerController {
         return "customer";
     }
 
-
+    public static void main(String[] args) {
+        System.out.println("不负韶华");
+    }
 
 }
